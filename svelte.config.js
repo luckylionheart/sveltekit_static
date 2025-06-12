0f0b9637430c62d2ffbbe7cfab80b5c5f3f3a57c
0~_~0 <77128380+luckylionheart@users.noreply.github.com>
2025-06-12 18:56:04 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,14 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+       fallback: 'index.html'
+    }),
     paths: {
-      base: '/sveltekit_static', // 🔁 Replace with your GitHub repo name
+      base: '/sveltekit_static',
     },
     prerender: {
-      entries: ['*']
+      entries: ['/sveltekit_static'] // 👈 Add this
     }
   }
 };
